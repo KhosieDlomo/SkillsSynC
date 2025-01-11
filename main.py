42918f502bef3c8bcc0d70037bfe3af8309daa10
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-01-12 01:10:23 +0200

    database URL and initializing

--- main.py
+++ main.py
@@ -6,19 +6,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 Config = {
+          'apiKey':os.getenv('API_KEY'),
+          'authDomain':os.getenv('AUTH_DOMAIN'),
+          'projectId': os.getenv('PROJECT_ID'),
+          'storageBucket':os.getenv('STORAGE_BUCKET'),
+          'messagingSenderId': os.getenv('MESSAGE_SENDER_ID'),
+          'appId': os.getenv('APP_ID'),
+          'measurementId': os.getenv('MEASUREMENT_ID'),
+          'databaseURL':os.getenv('DATABASE_URL')
+          }
+firebase=pyrebase.initialize_app(Config)
+auth=firebase.auth()
 
-  'apiKey':os.getenv('API_KEY'),
+def signin():
+    email = input("Enter your email: ")
+    password = input('Enter your Password:')
+    try:
+        signin = auth.sign_in_with_email_and_password(email, password)
+        print('successfully signedIn')
+    except Exception as e:
+        print('Invalid credentials')
+    return
 
-  'authDomain':os.getenv('AUTH_DOMAIN'),
+def signup():
+    email = input("Enter your email: ")
+    password = input('Enter your Password:')
+    try:
+        user = auth.create_user_with_email_and_password(email,password)
+    except Exception as e:
+        print('Email already been used')
+    return
 
-  'projectId': os.getenv('PROJECT_ID'),
-
-  'storageBucket':os.getenv('STORAGE_BUCKET'),
-
-  'messagingSenderId': os.getenv('MESSAGE_SENDER_ID'),
-
-  'appId': os.getenv('APP_ID'),
-
-  'measurementId': os.getenv('MEASUREMENT_ID') 
-
-}
