d40429fffd4eaebc1cc3230ca2adc1aa85566975
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-01-14 17:36:07 +0200

    Validation

--- main.py
+++ main.py
@@ -53,11 +53,18 @@ def signup():
     """Hello, Join us by signing up"""
     name = input("Enter your Full Name: ")
     email = input("Enter your email: ")
-    password = pwinput.pwinput(prompt='Enter your Password: ', mask='#')
-    confirm_password = pwinput.pwinput(prompt='Confirm Your password: ', mask='#')
-    if password != confirm_password:
-        click.echo('Error: Password do not match!')
-        return
+
+    while True:
+        password = pwinput.pwinput(prompt='Enter your Password: ', mask='#')
+        confirm_password = pwinput.pwinput(prompt='Confirm Your password: ', mask='#')
+        if password != confirm_password:
+            click.echo('Error: Password do not match!')
+            continue
+        validation = valid_input(password, email)
+        if validation[0] is not True:
+            click.echo(f'Error! Invalid Password {validation[1] or ""}')
+        else:
+            break
     try:
         user = auth.create_user_with_email_and_password(email,password)
         auth.send_email_verification(user['idToken'])
@@ -84,6 +91,39 @@ def signout():
     except Exception as e:
         click.echo(f'Run into an issue while signing out: {e}')
 
+def valid_input(password, email):
+    if len(password) < 8:
+        return False
+
+    has_digit = False
+    has_lowercase = False
+    has_uppercase = False
+    has_specialChar = False
+
+    for char in password:
+        if char.isdigit():
+            has_digit = True
+        if char.islower():
+            has_lowercase = True
+        if char.isupper():
+            has_uppercase = True
+        if not char.isalnum(): 
+            has_specialChar = True
+
+    if not has_digit:
+        return False
+    if not has_lowercase:
+        return False
+    if not has_uppercase:
+        return False
+    if not has_specialChar:
+        return False
+   
+    if '@gmail.com' not in email:
+        return False
+
+    return True
+    
 cli.add_command(signin)
 cli.add_command(signup)
 cli.add_command(reset_password)
