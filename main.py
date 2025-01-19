e447e5de7aefabf9814aab393fbdff31e26f897a
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-01-19 22:39:56 +0200

    imports

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow 
 from googleapiclient.discovery import build  
 from google.auth.transport.requests import Request
+from google.oauth2 import service_account
 from googleapiclient.errors import HttpError
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+SERVICE_ACCOUNT_FILE = 'path'
 SCOPES = ['https://www.googleapis.com/auth/calendar',
           'https://www.googleapis.com/auth/calendar.events'
          ]
@@ -144,14 +146,15 @@ def bookings():
         if cred and cred.expired and cred.refresh_token:
             cred.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json',SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             cred = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(cred.to_json())
     try:
         service = build('calendar', 'v3', credentials=cred)
         now = datetime.datetime.now().isoformat() + 'Z'
-        event_result = service.events().list(calendarId='primary', timeMin=now, maxResults=10, singleEvents= True, orderBy='startTime')
+        event_result = service.events().list(calendarId='primary', timeMin=now, maxResults=10, singleEvents= True, orderBy='startTime').execute()
+        events = event_result.get('items', [])
     except HttpError as error:
         click.echo(f'An error occured: {error}')
 cli.add_command(signin)
