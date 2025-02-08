beca5aa6f16e44a30ab9dc361d1352e54a723497
Khosi Dlomo <makhosijdlomo97@gmail.com>
2025-02-08 22:46:20 +0200

    peer function

--- availability.py
+++ availability.py
@@ -8,10 +8,23 @@ def listing_available_mentors():
     if not mentors:
         click.echo('No mentors available at the moment.')
         return []
-    click.echo('Available Mentors: ')
+    click.echo('Available Mentors...')
     mentor_lst = []
     for mentor in mentors:
         mentor_info = mentor.to_dict()
         click.echo(f' - Name: {mentor_info['name']}, Email: {mentor_info['email']},ID: {mentor.id}')
         mentor_lst.append({'id': mentor.id, 'name': mentor_info['name'], 'email': mentor_info['email']})
         return mentor_lst
+    
+def available_peers():
+    '''Function to display all the available peers'''
+    peers = db.collection('user').where('role', '==', 'peer').get()
+    if not peers:
+        click.echo('No peers available at the moment.')
+        return []
+    click.echo('Available Peers...')
+    peers_lst = []
+    for peer in peers:
+        peer_info = peer.to_dict()
+        click.echo(f' - Name: {peer_info['name']}, Email: {peer_info['email']}, ID: {peer.id}')
+        peers_lst.append({"id": peer.id, 'name': peer_info['name'],'email': peer_info['Email']})
